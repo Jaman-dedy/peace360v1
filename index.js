2155ffb2b06bd4bd29d7c05f3171b5ae3274c9e5
Jaman Dedy <jeandedieuam@gmail.com>
2020-01-22 19:48:10 +0200

    adding cors

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 import connectDB from './config/db';
 import router from './routes/app';
 
 const app = express();
+app.use(cors());
 
 //Connect to the db
 connectDB();
 
 //Init Middlware
 
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
 app.use(router);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server up on port ${port}`));
 
-export default app; 
+export default app;
