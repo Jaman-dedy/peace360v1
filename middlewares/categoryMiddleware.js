acb6e1256e92f80afc09559d7d21d4dddfc0e4bf
MBenedicte <mbenedicte20@gmail.com>
2020-03-11 04:39:31 +0200

    fix categories

--- middlewares/categoryMiddleware.js
+++ middlewares/categoryMiddleware.js
@@ -54,30 +54,4 @@ const updateCategoryMiddleware = async (req, res, next) => {
     });
   }
 };
-const updateCategoryMiddleware = async (req, res, next) => {
-  const { categoryTitle } = req.body;
-  const { title } = req.params;
-  try {
-    let findCategory = await Category.findOne({ categoryTitle: title });
-
-    if (!findCategory) {
-      res.status(statusCode.EXIST).json({
-        errors: [{ message: 'Category does not exist' }]
-      });
-    } else {
-      if (categoryTitle === findCategory.categoryTitle) {
-        next();
-      } else {
-        res.status(statusCode.EXIST).json({
-          errors: [{ message: 'Category title already exist' }]
-        });
-      }
-    }
-  } catch (error) {
-    res.status(statusCode.SERVER_ERROR).json({
-      status: statusCode.SERVER_ERROR,
-      message: error.message
-    });
-  }
-};
 export { checkCategoryExist, checkCategoryNotExist, updateCategoryMiddleware };
