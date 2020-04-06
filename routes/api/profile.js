dbfb79bd4bd05c3135fe616a9126348058fb8c99
MBenedicte <mbenedicte20@gmail.com>
2020-04-06 23:37:29 +0200

    fix profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,15 @@
-import express from "express";
-import Profile from "../../controllers/profile";
-import auth from "../../middlewares/auth";
-import { validateProfile } from "../../middlewares/validateProfile";
+import express from 'express';
+import Profile from '../../controllers/profile';
+import auth from '../../middlewares/auth';
+import { validateProfile } from '../../middlewares/validateProfile';
 
 const profile = new Profile();
 
 const router = express.Router();
 
-router.get("/me", auth, profile.currentUserProfile);
-router.get("/:userId", auth, profile.givenUserProfile);
-router.post("/", [auth, validateProfile], profile.createOrUpdateProfile);
+router.get('/me', auth, profile.currentUserProfile);
+router.get('/:userId', auth, profile.givenUserProfile);
+
+router.post('/create', [auth, validateProfile], profile.createProfile);
+router.put('/edit', [auth, validateProfile], profile.updateProfile);
 export default router;
