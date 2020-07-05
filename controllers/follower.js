5114dca0659a825da7c9698185b5753a556107e1
Jaman-dedy <jeandedieuam@gmail.com>
2020-07-05 20:15:21 +0200

    fixing following func

--- controllers/follower.js
+++ controllers/follower.js
@@ -1,73 +1,51 @@
 import User from '../models/User';
-import Article from '../models/Article';
 import Followers from '../models/Followers';
 
 class FollowerController {
   async followUser(req, res) {
+
     try {
-      const currentArticle = await Article.findById(req.params.article_id);
-
-      const user = currentArticle.user;
-      const userToFollow = await User.findById(user.id);
-
-      const followers = new Followers();
-      const follower = await Followers.find();
-
-      const followedBy = {
-        user: req.user.id,
-        username: req.user.username,
-        avatar: req.user.avatar,
-      };
-
-      const followedUser = {
-        user,
-        username: userToFollow.username,
-        avatar: userToFollow.avatar,
-      };
-      if (user.id == req.user.id) {
-        return res.status(401).json({
-          status: 401,
-          msg: 'unauthorized action, you can not follow yourself',
+      const followingUser = await User.findById(req.params.userId);
+      const followings = await Followers.find({});
+      const followers = new Followers({
+        following: req.params.userId,
+        follower: req.user.id
+      });
+      let redundant = [];
+      const isFollowing =
+        followings && followings.map(user => {
+          if (user.follower.toString() === req.user.id.toString() && user.following.toString() === req.params.userId) {
+            redundant.push(user)
+          }
         });
-      }
-
-      const myFollowings = follower.filter((follower) =>
-        follower.followedBy.find(
-          (followedBy) => followedBy.user.toString() === req.user.id
-        )
-      );
-
-      myFollowings;
-
-      if (myFollowings.length) {
-        const unFollowUser = myFollowings.find((following) =>
-          following.followedUser.find(
-            (unFollow) => unFollow.username === user.username
-          )
-        );
-
-        if (unFollowUser) {
-          await Followers.findByIdAndDelete(unFollowUser.id);
+      if (redundant.length) {
+        if (redundant[0].follower.toString() === req.user.id) {
+          try {
+            await Followers.findOneAndDelete({
+              follower: req.user.id,
+              following: req.params.userId
+            });
+          } catch (error) {
+            throw error
+          }
           return res.status(200).json({
             status: 200,
-            msg: `You have successfully unfollow ${followedUser.username}`,
-            user: followedUser.user,
-            state: 'unfollow',
-          });
+            msg: `You've successfully unfollow ${followingUser.username}`,
+            user: followingUser,
+            state: 'unfollow'
+          })
         }
       }
 
-      followers.followedBy.unshift(followedBy);
-      followers.followedUser.unshift(followedUser.user);
-
       await followers.save();
 
-      res.status(201).json({
+      return res.status(201).json({
         status: 201,
-        msg: `Thanks for following ${followedUser.username}`,
-        user: followedUser.user,
+        msg: `Thanks for following ${followingUser.username}`,
+        user: followingUser,
         state: 'follow',
       });
+
     } catch (error) {
       return res.status(500).json({
         status: 500,
@@ -77,25 +55,17 @@ class FollowerController {
   }
   async getFollowers(req, res) {
     try {
-      const followers = await Followers.find();
+      const followers = await Followers.find().populate('following', ['avatar', 'username']).populate('follower', ['avatar', 'username']);
 
-      const followersList = followers.filter((follower) => {
-        return follower.followedUser.find(
-          (followedUser) => followedUser._id.toString() === req.user.id
-        );
-      });
-      const myFollowers = followers.map((followers) => {
-        return followers.followedBy.find((flw) => {
-          return flw;
-        });
-      });
+      const myFollowers = followers.filter(myFollower => {
+        return myFollower.following._id.toString() === req.user.id
+      })
       if (!myFollowers.length) {
         return res.status(404).json({
           status: 404,
-          msg: 'Oops, you do not have any follower currently',
-        });
+          msg: 'Oops, you do not have any follower currently'
+        })
       }
-
       return res.status(200).json({
         status: 200,
         myFollowers,
@@ -109,26 +79,17 @@ class FollowerController {
   }
   async getFollowings(req, res) {
     try {
-      const followers = await Followers.find();
+      const followings = await Followers.find().populate('following', ['avatar', 'username']).populate('follower', ['avatar', 'username']);
 
-      const followingsList = followers.filter((following) =>
-        following.followedBy.find(
-          (followedBy) => followedBy.user.toString() === req.user.id
-        )
-      );
-      if (!followingsList.length) {
+      const myFollowings = followings.filter(myFollowing => {
+        return myFollowing.follower._id.toString() === req.user.id
+      })
+      if (!myFollowings.length) {
         return res.status(404).json({
           status: 404,
-          msg: 'Oops, currently you are not following any user',
-        });
+          msg: 'Oops, you do not currently followed'
+        })
       }
-
-      const myFollowings = followingsList.map((followings) => {
-        return followings.followedUser.find((flw) => {
-          return flw;
-        });
-      });
-
       return res.status(200).json({
         status: 200,
         myFollowings,
--- routes/api/users.js
+++ routes/api/users.js
@@ -5,9 +5,14 @@ import {
   validateUser,
   validateUserLogin
 } from '../../middlewares/validateUser';
-import { checkUser, checkUserLogin } from '../../middlewares/checkUser';
+import {
+  checkUser,
+  checkUserLogin
+} from '../../middlewares/checkUser';
 import auth from '../../middlewares/auth';
-import { checkFollowers } from '../../middlewares/checkFollowers';
+import {
+  checkFollowers
+} from '../../middlewares/checkFollowers';
 
 
 const follower = new Follower();
@@ -18,7 +23,7 @@ router.post('/', validateUser, checkUser, users.signup);
 router.post('/login', validateUserLogin, checkUserLogin, users.login);
 router.post('/send-email', users.checkEmail);
 router.put('/reset-password', users.resetPassword);
-router.put('/follow/:article_id', [auth], follower.followUser);
+router.put('/follow/:userId', [auth], follower.followUser);
 router.get('/followers', auth, follower.getFollowers);
 router.get('/followings', auth, follower.getFollowings);
 router.put('/updateImage', auth, users.updateUserImage);
